16ab5ef2c1914deca8a745653bf2c9a39ac718d0
Дяченко Артем Петрович <ADyachenko3@alfabank.ru>
2020-08-20 15:53:25 +0300

    hometask 4: additional tasks - error fixing

--- app/src/main/java/ru/skillbranch/skillarticles/data/delegates/PrefDelegate.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/data/delegates/PrefDelegate.kt
@@ -5,16 +5,37 @@ import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
 
 class PrefDelegate<T>(private val defaultValue: T) : ReadWriteProperty<PrefManager, T?> {
-
     override fun getValue(thisRef: PrefManager, property: KProperty<*>): T? {
         val key = property.name
-        return thisRef.get(key, defaultValue)
+        with(thisRef.preferences) {
+            return when (defaultValue) {
+                is Boolean -> (getBoolean(key, defaultValue) as? T) ?: defaultValue
+                is String -> (getString(key, defaultValue) as? T) ?: defaultValue
+                is Int -> (getInt(key, defaultValue) as? T) ?: defaultValue
+                is Long -> (getLong(key, defaultValue) as? T) ?: defaultValue
+                is Float -> (getFloat(key, defaultValue) as? T) ?: defaultValue
+                else -> throw NotFoundRealizationException(defaultValue)
+            }
+        }
     }
 
     override fun setValue(thisRef: PrefManager, property: KProperty<*>, value: T?) {
         val key = property.name
-        thisRef.put(key, value)
+        with(thisRef.preferences.edit()) {
+            when (value) {
+                is Boolean -> putBoolean(key, value)
+                is String -> putString(key, value)
+                is Int -> putInt(key, value)
+                is Long -> putLong(key, value)
+                is Float -> putFloat(key, value)
+                else -> throw NotFoundRealizationException(value)
+            }.apply()
+        }
     }
+
+    class NotFoundRealizationException(defValue: Any?) :
+        Exception("not found realization for $defValue")
+
 }
 
 
--- app/src/main/java/ru/skillbranch/skillarticles/data/local/PrefManager.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/data/local/PrefManager.kt
@@ -2,45 +2,14 @@ package ru.skillbranch.skillarticles.data.local
 
 import android.content.Context
 import android.content.SharedPreferences
-import androidx.annotation.VisibleForTesting
 import androidx.preference.PreferenceManager
 
-class PrefManager(context: Context) {
-    @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
-    private val preferences: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
+open class PrefManager(context: Context) {
+
+    val preferences: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
 
     fun clearAll() {
         preferences.edit().clear().apply()
     }
-
-    fun <T> put(key: String, value: T) {
-        with(preferences.edit()) {
-            when (value) {
-                is Boolean -> putBoolean(key, value)
-                is String -> putString(key, value)
-                is Int -> putInt(key, value)
-                is Long -> putLong(key, value)
-                is Float -> putFloat(key, value)
-                else -> throw NotFoundRealizationException(value)
-            }.apply()
-        }
-    }
-
-    @Suppress("UNCHECKED_CAST")
-    fun <T> get(key: String, defValue: T): T? {
-        with(preferences) {
-            return when (defValue) {
-                is Boolean -> (getBoolean(key, defValue) as? T) ?: defValue
-                is String -> (getString(key, defValue) as? T) ?: defValue
-                is Int -> (getInt(key, defValue) as? T) ?: defValue
-                is Long -> (getLong(key, defValue) as? T) ?: defValue
-                is Float -> (getFloat(key, defValue) as? T) ?: defValue
-                else -> throw NotFoundRealizationException(defValue)
-            }
-        }
-    }
-
-    class NotFoundRealizationException(defValue: Any?) :
-        Exception("not found realization for $defValue")
 }
 
